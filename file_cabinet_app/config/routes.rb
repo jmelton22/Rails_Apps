9c1c018d233725c42eeac0a723f1505ad936b159
jmelton22 <jmelton22@gmail.com>
2019-06-30 10:52:07 -0400

    Changed root view for authenticated users

--- file_cabinet_app/config/routes.rb
+++ file_cabinet_app/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :documents
 
+  authenticated :user do
+    root 'documents#index'
+  end
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
