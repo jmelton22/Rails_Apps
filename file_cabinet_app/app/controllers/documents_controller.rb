0245843248a894c6ab3188e815d7d69283a5e220
jmelton22 <jmelton22@gmail.com>
2019-06-29 20:52:35 -0400

    Generated Document controller

---
+++ file_cabinet_app/app/controllers/documents_controller.rb
@@ -0,0 +1,37 @@
+class DocumentsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+end
+
+private
+  def doc_params
+
+  end
+
+  def find_doc
+
+  end
