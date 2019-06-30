ebc9c406d0308c0ce96e7b5c0cf415e67e7e6ee7
jmelton22 <jmelton22@gmail.com>
2019-06-29 21:02:49 -0400

    Added new view and form, updated new and create methods

--- file_cabinet_app/app/controllers/documents_controller.rb
+++ file_cabinet_app/app/controllers/documents_controller.rb
@@ -8,11 +8,17 @@ class DocumentsController < ApplicationController
   end
 
   def new
-
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(doc_params)
 
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -29,7 +35,7 @@ end
 
 private
   def doc_params
-
+    params.require(:document).permit(:title, :body)
   end
 
   def find_doc
