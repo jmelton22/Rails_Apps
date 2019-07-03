d8462261bbc000e7982bafe910da107675cc14f5
jmelton22 <jmelton22@gmail.com>
2019-07-03 17:24:49 -0400

    Added execJS gem for Javascript runtime

--- file_cabinet_app/config/boot.rb
+++ file_cabinet_app/config/boot.rb
@@ -1,6 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-# Make ExecJs use NodeJs
-ENV['EXECJS_RUNTIME'] = 'Node'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
